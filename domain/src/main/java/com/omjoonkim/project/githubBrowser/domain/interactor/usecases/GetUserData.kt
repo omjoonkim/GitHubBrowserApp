af3f276343a8816e80e99ab3fcb0f73f01e97bfd
Omjoonkim <omjoonkim@gmail.com>
2019-07-04 11:05:21 +0900

    Add missing code before commit

--- domain/src/main/java/com/omjoonkim/project/githubBrowser/domain/interactor/usecases/GetUserData.kt
+++ domain/src/main/java/com/omjoonkim/project/githubBrowser/domain/interactor/usecases/GetUserData.kt
@@ -3,21 +3,23 @@ package com.omjoonkim.project.githubBrowser.domain.interactor.usecases
 import com.omjoonkim.project.githubBrowser.domain.entity.Repo
 import com.omjoonkim.project.githubBrowser.domain.entity.User
 import com.omjoonkim.project.githubBrowser.domain.interactor.SingleUseCase
-import com.omjoonkim.project.githubBrowser.domain.repository.GitHubBrowserRepository
+import com.omjoonkim.project.githubBrowser.domain.repository.RepoRepository
+import com.omjoonkim.project.githubBrowser.domain.repository.UserRepository
 import com.omjoonkim.project.githubBrowser.domain.schedulers.SchedulersProvider
 import io.reactivex.Single
 import io.reactivex.functions.BiFunction
 
 class GetUserData(
-    private val gitHubRepository: GitHubBrowserRepository,
+    private val userRepository: UserRepository,
+    private val repoRepository: RepoRepository,
     schedulersProvider: SchedulersProvider
 ) : SingleUseCase<Pair<User, List<Repo>>, String>(
     schedulersProvider
 ) {
     override fun buildUseCaseSingle(userName: String): Single<Pair<User, List<Repo>>> =
         Single.zip(
-            gitHubRepository.getUserInfo(userName),
-            gitHubRepository.getUserRepos(userName),
+            userRepository.getUserInfo(userName),
+            repoRepository.getRepos(userName),
             BiFunction<User, List<Repo>, Pair<User, List<Repo>>> { t1, t2 ->
                 Pair(t1, t2.sortedByDescending { it.starCount })
             }
