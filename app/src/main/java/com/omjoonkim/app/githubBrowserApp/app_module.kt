2bd5bdf454c2d62b534d9a04e0c11069c826da2a
Omjoonkim <omjoonkim@gmail.com>
2019-07-04 11:07:34 +0900

    Define improved injection

--- app/src/main/java/com/omjoonkim/app/githubBrowserApp/app_module.kt
+++ app/src/main/java/com/omjoonkim/app/githubBrowserApp/app_module.kt
@@ -1,20 +1,22 @@
 package com.omjoonkim.app.githubBrowserApp
 
-import com.omjoonkim.app.githubBrowserApp.network.ApiServiceFactory
-import com.omjoonkim.app.githubBrowserApp.network.GithubService
-import com.omjoonkim.app.githubBrowserApp.repository.RepoRepository
-import com.omjoonkim.app.githubBrowserApp.repository.RepoRepositoryImpl
 import com.omjoonkim.app.githubBrowserApp.ui.repo.RepoDetailPresenter
 import com.omjoonkim.app.githubBrowserApp.ui.repo.RepoDetailView
 import com.omjoonkim.app.githubBrowserApp.viewmodel.MainViewModel
 import com.omjoonkim.app.githubBrowserApp.viewmodel.SearchViewModel
-import com.omjoonkim.project.githubBrowser.data.interactor.GithubBrowserDataSource
-import com.omjoonkim.project.githubBrowser.data.repository.GithubBrowserRemote
+import com.omjoonkim.project.githubBrowser.data.interactor.ForkDataRepository
+import com.omjoonkim.project.githubBrowser.data.interactor.RepoDataRepository
+import com.omjoonkim.project.githubBrowser.data.interactor.UserDataRepository
+import com.omjoonkim.project.githubBrowser.data.source.GithubBrowserRemote
+import com.omjoonkim.project.githubBrowser.domain.interactor.usecases.GetRepoDetail
 import com.omjoonkim.project.githubBrowser.domain.interactor.usecases.GetUserData
-import com.omjoonkim.project.githubBrowser.domain.repository.GitHubBrowserRepository
+import com.omjoonkim.project.githubBrowser.domain.repository.ForkRepository
+import com.omjoonkim.project.githubBrowser.domain.repository.RepoRepository
+import com.omjoonkim.project.githubBrowser.domain.repository.UserRepository
 import com.omjoonkim.project.githubBrowser.domain.schedulers.SchedulersProvider
 import com.omjoonkim.project.githubBrowser.remote.GithubBrowserRemoteImpl
 import com.omjoonkim.project.githubBrowser.remote.GithubBrowserServiceFactory
+import com.omjoonkim.project.githubBrowser.remote.mapper.ForkEntityMapper
 import com.omjoonkim.project.githubBrowser.remote.mapper.RepoEntityMapper
 import com.omjoonkim.project.githubBrowser.remote.mapper.UserEntityMapper
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -32,17 +34,19 @@ val myModule: Module = module {
     single { AppSchedulerProvider() as SchedulersProvider }
 
     //domain
-    factory { GetUserData(get(), get()) }
+    factory { GetUserData(get(), get(), get()) }
+    factory { GetRepoDetail(get(), get(), get()) }
 
     //data
-    single { GithubBrowserDataSource(get()) as GitHubBrowserRepository }
-    single { RepoRepositoryImpl(get()) as RepoRepository }
-    single { ApiServiceFactory.makeGithubBrowserService(BuildConfig.DEBUG, "https://api.github.com") as GithubService }
+    single { UserDataRepository(get()) as UserRepository }
+    single { RepoDataRepository(get()) as RepoRepository }
+    single { ForkDataRepository(get()) as ForkRepository }
 
     //remote
-    single { GithubBrowserRemoteImpl(get(), get(), get()) as GithubBrowserRemote }
+    single { GithubBrowserRemoteImpl(get(), get(), get(), get()) as GithubBrowserRemote }
     single { RepoEntityMapper() }
     single { UserEntityMapper() }
+    single { ForkEntityMapper(get()) }
     single {
         GithubBrowserServiceFactory.makeGithubBrowserService(
             BuildConfig.DEBUG,
